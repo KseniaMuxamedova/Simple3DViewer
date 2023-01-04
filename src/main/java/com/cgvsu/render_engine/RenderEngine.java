72e91c60a2c05405f2b84a37290e678368bcdbe6
KseniaMyh <vy33frb8>
2023-01-04 16:19:37 +0300

    С кнопкой заполнения полигонов 2

--- src/main/java/com/cgvsu/render_engine/RenderEngine.java
+++ src/main/java/com/cgvsu/render_engine/RenderEngine.java
@@ -13,7 +13,7 @@ public class RenderEngine {
     public static void render(
             final GraphicsContext graphicsContext,
             final Camera camera,
-            final Model mesh,
+            final ArrayList<Model> models,
             final int width,
             final int height)
     {
@@ -25,34 +25,37 @@ public class RenderEngine {
         modelViewProjectionMatrix.mul(viewMatrix);
         modelViewProjectionMatrix.mul(projectionMatrix);
 
-        final int nPolygons = mesh.polygons.size();
-        for (int polygonInd = 0; polygonInd < nPolygons; ++polygonInd) {
-            final int nVerticesInPolygon = mesh.polygons.get(polygonInd).getVertexIndices().size();
+        for (int model = 0; model < models.size(); model++){
+            final int nPolygons = models.get(model).polygons.size();
+            for (int polygonInd = 0; polygonInd < nPolygons; ++polygonInd) {
+                final int nVerticesInPolygon = models.get(model).polygons.get(polygonInd).getVertexIndices().size();
 
-            ArrayList<Point2f> resultPoints = new ArrayList<>();
-            for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
-                Vector3f vertex = mesh.vertices.get(mesh.polygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
+                ArrayList<Point2f> resultPoints = new ArrayList<>();
+                for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
+                    Vector3f vertex = models.get(model).vertices.get(models.get(model).polygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
 
-                javax.vecmath.Vector3f vertexVecmath = new javax.vecmath.Vector3f(vertex.x, vertex.y, vertex.z);
+                    javax.vecmath.Vector3f vertexVecmath = new javax.vecmath.Vector3f(vertex.x, vertex.y, vertex.z);
 
-                Point2f resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVecmath), width, height);
-                resultPoints.add(resultPoint);
+                    Point2f resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVecmath), width, height);
+                    resultPoints.add(resultPoint);
+                }
+
+                for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
+                    graphicsContext.strokeLine(
+                            resultPoints.get(vertexInPolygonInd - 1).x,
+                            resultPoints.get(vertexInPolygonInd - 1).y,
+                            resultPoints.get(vertexInPolygonInd).x,
+                            resultPoints.get(vertexInPolygonInd).y);
+                }
+
+                if (nVerticesInPolygon > 0)
+                    graphicsContext.strokeLine(
+                            resultPoints.get(nVerticesInPolygon - 1).x,
+                            resultPoints.get(nVerticesInPolygon - 1).y,
+                            resultPoints.get(0).x,
+                            resultPoints.get(0).y);
             }
-
-            for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
-                graphicsContext.strokeLine(
-                        resultPoints.get(vertexInPolygonInd - 1).x,
-                        resultPoints.get(vertexInPolygonInd - 1).y,
-                        resultPoints.get(vertexInPolygonInd).x,
-                        resultPoints.get(vertexInPolygonInd).y);
-            }
-
-            if (nVerticesInPolygon > 0)
-                graphicsContext.strokeLine(
-                        resultPoints.get(nVerticesInPolygon - 1).x,
-                        resultPoints.get(nVerticesInPolygon - 1).y,
-                        resultPoints.get(0).x,
-                        resultPoints.get(0).y);
         }
+
     }
 }
